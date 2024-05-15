832ff45e07cc2b768350e6aad8567e470215bf02
Paulo Ziembick <102979940+ziembick@users.noreply.github.com>
2024-05-15 16:13:56 -0300

    Header full (#2)
    * header
    
    * header full

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,15 @@
+
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="pt-80 pb-80">
+      <div className="container">
+        <div className="h1">Headless Blog</div>
+      </div>
+    </header>
+  );
+
 import React from 'react'
 
 export default function Header() {
@@ -6,4 +18,5 @@ export default function Header() {
       
     </div>
   )
+
 }
