0d56947172971caaca4e0ab5ece99de1a7ea69ca
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-15 15:59:54 -0300

    footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className='container'>
+            <hr className='mb-30'/>
+            <p>&copy; Rode Ziembick</p>
+        </div>
+    </footer>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,19 @@ import type { Metadata } from "next";
 import { Inter, Lato, Open_Sans } from "next/font/google";
 import "../styles/styles.sass";
 import Header from "./components/header";
+import Footer from "./components/footer";
 // import "./globals.css";
 
-const lato = Lato({ subsets: ["latin"], weight: ["400", "700", "900"], variable: '--font-lato' });
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700", "900"],
+  variable: "--font-lato",
+});
 
-const openSans = Open_Sans ({subsets: ['latin'], variable: '--font-open-sans'})
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+});
 
 export const metadata: Metadata = {
   title: "Rode Ziembick - Psicanalista",
@@ -20,11 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={` ${openSans.className} ${openSans.variable} ${lato.variable}`}>
-        
+      <body
+        className={` ${openSans.className} ${openSans.variable} ${lato.variable}`}
+      >
         <Header />
         {children}
-        </body>
+        <Footer />
+      </body>
     </html>
   );
 }
