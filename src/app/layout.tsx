9ec3c2ba904ee2a3d92b554f810d3a220fb10983
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-21 15:04:13 -0300

    correct routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
         className={` ${openSans.className} ${openSans.variable} ${lato.variable}`}
       >
         <Header />
-        <Headless />
         {children}
         <Footer />
       </body>
