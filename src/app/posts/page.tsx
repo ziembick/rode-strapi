01d76bf226d3d4646b28a1446df10e66dff022a8
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-24 14:38:52 -0300

    styling posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,55 +1,55 @@
 import {getAllArticles} from '@/../lib/api'
 import Image from 'next/image';
 import Link from 'next/link';
-
+import styles from './posts.module.sass'
 
 export default async function Home() {
   const articles = await getAllArticles();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <section className="w-full pt-12">
-        <div className="mx-auto container space-y-12 px-4 md:px-6">
-          <div className="flex flex-col items-center justify-center space-y-4 text-center">
-            <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+    <main className={`${styles.contentMain} container`}>
+      <section className={styles.elementSection}>
+        <div className={styles.mainDiv}>
+          <div className={styles.headerDiv}>
+            <div className={styles.textDiv}>
+              <h1 className={styles.textH1}>
                 Welcome to our Knowledge Base
               </h1>
-              <p className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
+              <p className={styles.ptag}>
                 Discover our latest articles and stay up to date with the newest
                 technologies, features, and trends.
               </p>
             </div>
           </div>
-          <div className="space-y-12">
-            <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <div className={styles.cardContainer}>
+            <div className={styles.card}>
               {articles && articles.map((article:any) => (
-                  <article key={article.sys.id} className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden">
+                  <article key={article.sys.id} className={styles.articleCard}>
                     <Image
                       alt="placeholder"
-                      className="aspect-[4/3] object-cover w-full"
+                      className={styles.image}
                       height="263"
                       src={article.articleImage.url}
                       width="350"
                     />
-                    <div className="flex-1 p-6">
+                    <div className={styles.linksDiv}>
                       <Link href={`/articles/${article.slug}`}>
-                        <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
+                        <h3 className={styles.articleTitle}>
                           {article.title}
                         </h3>
                       </Link>
-                      <div className="inline-block rounded-full bg-zinc-100 px-3 py-1 text-sm font-semibold text-zinc-800">
+                      <div className={styles.category}>
                         {article.categoryName}
                       </div>
-                      <p className="max-w-none text-zinc-500 mt-4 mb-2 text-sm dark:text-zinc-400">
+                      <p className={styles.articleSummary}>
                         {article.summary}
                       </p>
-                      <p className="max-w-none text-zinc-600 mt-2 mb-2 text-sm font-bold dark:text-zinc-400">
+                      <p className={styles.articleAuthor}>
                         Written by: {article.authorName}
                       </p>
-                      <div className="flex justify-end">
+                      <div className={styles.readMore}>
                         <Link
-                          className="inline-flex h-10 items-center justify-center text-sm font-medium"
+                          className={styles.linkReadMore}
                           href={`/articles/${article.slug}`}
                         >
                           Read More →
