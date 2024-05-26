26ba695958b878a363e1f43cd7e93c5ea1f23d51
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-26 02:41:01 -0300

    posts styling

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getAllArticles, getArticle } from "@/../lib/api";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import styles from './article.module.sass'
 
 interface KnowledgeArticleProps {
   params: {
@@ -27,28 +28,28 @@ export default async function KnowledgeArticle({
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <section className="w-full">
-        <div className="container space-y-12 px-4 md:px-6">
-          <div className="space-y-4">
-            <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
+    <main className={`${styles.mainContainer} container`}>
+      <section className={styles.secao}>
+        <div className={styles.mainDiv}>
+          <div className={styles.article}>
+            <h1 className={styles.articleTitle}>
               {article.title}
             </h1>
-            <p className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
+            <p className={styles.articleSummary}>
               {article.summary}
             </p>
           </div>
-          <div className="space-y-8 lg:space-y-10">
+          <div className={styles.imageDiv}>
             <Image
               alt="Article Image"
-              className="aspect-video w-full overflow-hidden rounded-xl object-cover"
+              className={styles.imageClass}
               height="365"
               src={article.articleImage.url}
               width="650"
             />
-            <div className="space-y-4 md:space-y-6">
-              <div className="space-y-2">
-                <div className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
+            <div className={styles.detailsDiv}>
+              <div className={styles.details}>
+                <div className={styles.detailsMain}>
                   {documentToReactComponents(article.details.json)}
                 </div>
               </div>
