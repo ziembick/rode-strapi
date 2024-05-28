a14183e3b467a25bb00a2b83090cf0fb97517d8d
Paulo Ziembick <102979940+ziembick@users.noreply.github.com>
2024-05-28 17:24:49 -0300

    Contentful events (#11)
    * contentful
    
    * contentful app rout graphql
    
    * styling posts page
    
    * posts styling
    
    * link whats
    
    * calendar events

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import BookAppointment from "../components/BookApointment";
 
 const Contato = () => {
   return (
     <div className="container">
       <div className="h1">Contato</div>
+      {/* <button className="mt-10">Agendar via calendario</button> */}
+      <BookAppointment />
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
 import { Baloo_2, Inter, Lato, Open_Sans } from "next/font/google";
 import "../styles/styles.sass";
-
 import Footer from "./components/footer";
-import Headless from "./components/headless";
 import { Header } from "./components/header";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { createClient } from "@supabase/supabase-js";
+import {
+  SessionContextProvider,
+  useSession,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import AppProviders from "./AppProviders";
+
 // import "./globals.css";
 
 const lato = Lato({
@@ -14,10 +21,10 @@ const lato = Lato({
 });
 
 const baloo = Baloo_2({
-  subsets: ['latin'],
-  weight:['400', '500', '600', '700', '800'],
-  variable: "--font-baloo2"
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-baloo2",
+});
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -29,20 +36,28 @@ export const metadata: Metadata = {
   description: "Rode Ziembick - Psicanalista",
 };
 
+const supabase = createClient(
+  "https://xlaljpesbddiizjsowua.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsYWxqcGVzYmRkaWl6anNvd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4NzEwMTksImV4cCI6MjAzMjQ0NzAxOX0.gqr4bdSI6nFqt3j-2UrW263w8gKsfdR0y6eap2VM6rU"
+);
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body
-        className={` ${openSans.className} ${openSans.variable} ${lato.variable} ${baloo.variable} ${baloo.className}`}
-      >
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <AppProviders>
+      <html lang="pt-BR">
+        <body
+          className={` ${openSans.className} ${openSans.variable} ${lato.variable} ${baloo.variable} ${baloo.className}`}
+        >
+          <Header />
+          {children}
+
+          <Footer />
+        </body>
+      </html>
+    </AppProviders>
   );
 }
