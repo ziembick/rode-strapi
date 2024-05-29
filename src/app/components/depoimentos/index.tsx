f441fbf1e5fabf3cab9f4f22a43660f8b6ab4663
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-29 17:18:07 -0300

    depoimentos page

--- src/app/components/depoimentos/index.tsx
+++ src/app/components/depoimentos/index.tsx
@@ -2,7 +2,18 @@ import Image from "next/image";
 import React from "react";
 import styles from "./depoimentos.module.sass";
 
+
+
 export default function Depoimentos() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptativeHeight: true,
+  };
+
   return (
     <div className={`${styles.depoimentos} container`}>
       <div className={styles.textContainer}>
@@ -12,20 +23,36 @@ export default function Depoimentos() {
           Comentários retirados do Google Meu Negócio
         </p>
       </div>
-      <div className={styles.commentContainer}>
-        <p className={styles.comentários}>
-          Profissional de ótima qualidade. Dr. Rode com sua calmaria, paciência
-          e competência em seus diagnósticos precisos consegue nos ajudar a
-          termos um melhor conhecimento de nossas condições psicológicas e
-          emocionais pra conseguirmos melhorar nossas idas em meio a grande
-          atribulações em que estamos vivendo atualmente em nossa vida
-          profissional e pessoal. Gratidão por tudo.
-        </p>
-        <div className={styles.userInfo}>
-        <Image src="/anon.webp" alt="User" width={40} height={40} />
-        <h6 className={styles.nome}>Nome</h6>
+
+        <div className={styles.commentContainer}>
+          <p className={styles.comentários}>
+            Profissional de ótima qualidade. Dr. Rode com sua calmaria,
+            paciência e competência em seus diagnósticos precisos consegue nos
+            ajudar a termos um melhor conhecimento de nossas condições
+            psicológicas e emocionais pra conseguirmos melhorar nossas idas em
+            meio a grande atribulações em que estamos vivendo atualmente em
+            nossa vida profissional e pessoal. Gratidão por tudo.
+          </p>
+          <div className={styles.userInfo}>
+            <Image src="/anon.webp" alt="User" width={40} height={40} />
+            <h6 className={styles.nome}>Nome</h6>
+          </div>
         </div>
-      </div>
+        <div className={styles.commentContainer}>
+          <p className={styles.comentários}>
+            Profissional de ótima qualidade. Dr. Rode com sua calmaria,
+            paciência e competência em seus diagnósticos precisos consegue nos
+            ajudar a termos um melhor conhecimento de nossas condições
+            psicológicas e emocionais pra conseguirmos melhorar nossas idas em
+            meio a grande atribulações em que estamos vivendo atualmente em
+            nossa vida profissional e pessoal. Gratidão por tudo.
+          </p>
+          <div className={styles.userInfo}>
+            <Image src="/anon.webp" alt="User" width={40} height={40} />
+            <h6 className={styles.nome}>Nome</h6>
+          </div>
+        </div>
+
     </div>
   );
 }
