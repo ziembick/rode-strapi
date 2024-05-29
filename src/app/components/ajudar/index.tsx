0c34e68cc6c7e36c504cb5f3c400fe26bf351a4e
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-29 16:49:16 -0300

    esqueleto da homepage

--- src/app/components/ajudar/index.tsx
+++ src/app/components/ajudar/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client'
+
+import React from "react";
+import BtnAgende from "../btnAgende";
+import styles from "./ajudar.module.sass"
 
 export default function Ajudar() {
   return (
-    <div className='container'>
-      <h1>Posso ajudar você</h1>
+    <div className={styles.ajudar}>
+      <h1 className={styles.ajudarTitle}>Posso ajudar você</h1>
+      <p className={`${styles.descriptionTitle} container`}>
+        Caso você se identifique com algum desses sintomas ou alguma dessas
+        situações, saiba que a <strong>Psicoterapia Jungiana</strong> pode ser
+        de grande ajuda para transformar a sua vidade de maneira signficativa!
+      </p>
+      <BtnAgende />
     </div>
-  )
+  );
 }
--- src/app/components/depoimentos/index.tsx
+++ src/app/components/depoimentos/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import styles from "./depoimentos.module.sass";
 
 export default function Depoimentos() {
   return (
-    <div className='container'>
-      <h1>Depoimentos</h1>
+    <div className={`${styles.depoimentos} container`}>
+      <div className={styles.textContainer}>
+        <h6 className={styles.dizem}>O que dizem os pacientes</h6>
+        <h1 className={styles.title}>Depoimentos</h1>
+        <p className={styles.description}>
+          Comentários retirados do Google Meu Negócio
+        </p>
+      </div>
+      <div className={styles.commentContainer}>
+        <p className={styles.comentários}>
+          Profissional de ótima qualidade. Dr. Rode com sua calmaria, paciência
+          e competência em seus diagnósticos precisos consegue nos ajudar a
+          termos um melhor conhecimento de nossas condições psicológicas e
+          emocionais pra conseguirmos melhorar nossas idas em meio a grande
+          atribulações em que estamos vivendo atualmente em nossa vida
+          profissional e pessoal. Gratidão por tudo.
+        </p>
+        <div className={styles.userInfo}>
+        <Image src="/anon.webp" alt="User" width={40} height={40} />
+        <h6 className={styles.nome}>Nome</h6>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
