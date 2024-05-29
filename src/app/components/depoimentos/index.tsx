cadd7b9cb2189a822e1e59c0a8ec73032ea1672d
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-29 17:25:26 -0300

    swiper

--- src/app/components/depoimentos/index.tsx
+++ src/app/components/depoimentos/index.tsx
@@ -1,19 +1,13 @@
+'use client'
+
 import Image from "next/image";
 import React from "react";
 import styles from "./depoimentos.module.sass";
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.css';
+import { Pagination } from 'swiper/modules';
 
 export default function Depoimentos() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptativeHeight: true,
-  };
-
   return (
     <div className={`${styles.depoimentos} container`}>
       <div className={styles.textContainer}>
@@ -23,36 +17,45 @@ export default function Depoimentos() {
           Comentários retirados do Google Meu Negócio
         </p>
       </div>
-
-        <div className={styles.commentContainer}>
-          <p className={styles.comentários}>
-            Profissional de ótima qualidade. Dr. Rode com sua calmaria,
-            paciência e competência em seus diagnósticos precisos consegue nos
-            ajudar a termos um melhor conhecimento de nossas condições
-            psicológicas e emocionais pra conseguirmos melhorar nossas idas em
-            meio a grande atribulações em que estamos vivendo atualmente em
-            nossa vida profissional e pessoal. Gratidão por tudo.
-          </p>
-          <div className={styles.userInfo}>
-            <Image src="/anon.webp" alt="User" width={40} height={40} />
-            <h6 className={styles.nome}>Nome</h6>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <div className={styles.commentContainer}>
+            <p className={styles.comentários}>
+              Profissional de ótima qualidade. Dr. Rode com sua calmaria,
+              paciência e competência em seus diagnósticos precisos consegue nos
+              ajudar a termos um melhor conhecimento de nossas condições
+              psicológicas e emocionais pra conseguirmos melhorar nossas idas em
+              meio a grande atribulações em que estamos vivendo atualmente em
+              nossa vida profissional e pessoal. Gratidão por tudo.
+            </p>
+            <div className={styles.userInfo}>
+              <Image src="/anon.webp" alt="User" width={40} height={40} />
+              <h6 className={styles.nome}>Nome</h6>
+            </div>
           </div>
-        </div>
-        <div className={styles.commentContainer}>
-          <p className={styles.comentários}>
-            Profissional de ótima qualidade. Dr. Rode com sua calmaria,
-            paciência e competência em seus diagnósticos precisos consegue nos
-            ajudar a termos um melhor conhecimento de nossas condições
-            psicológicas e emocionais pra conseguirmos melhorar nossas idas em
-            meio a grande atribulações em que estamos vivendo atualmente em
-            nossa vida profissional e pessoal. Gratidão por tudo.
-          </p>
-          <div className={styles.userInfo}>
-            <Image src="/anon.webp" alt="User" width={40} height={40} />
-            <h6 className={styles.nome}>Nome</h6>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.commentContainer}>
+            <p className={styles.comentários}>
+              Profissional de ótima qualidade. Dr. Rode com sua calmaria,
+              paciência e competência em seus diagnósticos precisos consegue nos
+              ajudar a termos um melhor conhecimento de nossas condições
+              psicológicas e emocionais pra conseguirmos melhorar nossas idas em
+              meio a grande atribulações em que estamos vivendo atualmente em
+              nossa vida profissional e pessoal. Gratidão por tudo.
+            </p>
+            <div className={styles.userInfo}>
+              <Image src="/anon.webp" alt="User" width={40} height={40} />
+              <h6 className={styles.nome}>Nome</h6>
+            </div>
           </div>
-        </div>
-
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
