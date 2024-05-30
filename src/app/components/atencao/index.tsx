2c2156eba70d9ed82eb1ee15bafc1230fbf510aa
Paulo Ziembick <102979940+ziembick@users.noreply.github.com>
2024-05-29 21:37:27 -0300

    Mainpage esqueleto (#14)
    * esqueleto da homepage
    
    * depoimentos page
    
    * swiper
    
    * quase tudo falta atencao, semifooter, contato e o que eh
    
    * esqueleto praticamente finalizado

---
+++ src/app/components/atencao/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./atencao.module.sass";
+
+export default function Atencao() {
+  return (
+    <div className={`${styles.atencao} container mt-10`}>
+      <div className={`${styles.imageContainer} mt-10`}>
+        <Image src="/atencao.webp" alt="Atencao" width={90} height={90} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Atenção</h1>
+        <p className={styles.description}>
+          Esse site não oferece atendimento imediato a paessoas em crise
+          suicida. Em caso de crise ligue para o <strong>CVV - 188</strong>. Em
+          caso de emergência, procure o hospital mais próximo. Havendo risco de
+          morte, ligue imediatamente para o <strong>SAMU - telefone 192</strong>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/components/depoimentos/index.tsx
+++ src/app/components/depoimentos/index.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+'use client'
+
+import Image from "next/image";
+import React from "react";
+import styles from "./depoimentos.module.sass";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.css';
+import { Pagination } from 'swiper/modules';
 
 export default function Depoimentos() {
   return (
-    <div className='container'>
-      <h1>Depoimentos</h1>
+    <div className={`${styles.depoimentos} container`}>
+      <div className={styles.textContainer}>
+        <h6 className={styles.dizem}>O que dizem os pacientes</h6>
+        <h1 className={styles.title}>Depoimentos</h1>
+        <p className={styles.description}>
+          Comentários retirados do Google Meu Negócio
+        </p>
+      </div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <div className={styles.commentContainer}>
+            <p className={styles.comentários}>
+              Profissional de ótima qualidade. Dr. Rode com sua calmaria,
+              paciência e competência em seus diagnósticos precisos consegue nos
+              ajudar a termos um melhor conhecimento de nossas condições
+              psicológicas e emocionais pra conseguirmos melhorar nossas idas em
+              meio a grande atribulações em que estamos vivendo atualmente em
+              nossa vida profissional e pessoal. Gratidão por tudo.
+            </p>
+            <div className={styles.userInfo}>
+              <Image src="/anon.webp" alt="User" width={40} height={40} />
+              <h6 className={styles.nome}>Nome</h6>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.commentContainer}>
+            <p className={styles.comentários}>
+              Profissional de ótima qualidade. Dr. Rode com sua calmaria,
+              paciência e competência em seus diagnósticos precisos consegue nos
+              ajudar a termos um melhor conhecimento de nossas condições
+              psicológicas e emocionais pra conseguirmos melhorar nossas idas em
+              meio a grande atribulações em que estamos vivendo atualmente em
+              nossa vida profissional e pessoal. Gratidão por tudo.
+            </p>
+            <div className={styles.userInfo}>
+              <Image src="/anon.webp" alt="User" width={40} height={40} />
+              <h6 className={styles.nome}>Nome</h6>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
-  )
+  );
 }
