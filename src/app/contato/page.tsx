d52cf2186351ab32e24f71531a103b2d536b479c
Paulo Ziembick <102979940+ziembick@users.noreply.github.com>
2024-06-05 01:12:04 -0300

    Final atencao (#18)
    * foto sobre mim nova
    
    * mudei as cores do sobre mim
    
    * sobre mim
    
    * alerta contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -4,10 +4,12 @@ import styles from "./contato.module.sass";
 
 const Contato = () => {
   return (
-    <div className="container">
-      <h1 className={styles.contato}>Contato</h1>
-      <div>
-        <BookAppointment />
+    <div className={styles.bgContainer}>
+      <div className="container">
+        <h1 className={styles.contato}>Contato</h1>
+        <div>
+          <BookAppointment />
+        </div>
       </div>
     </div>
   );
