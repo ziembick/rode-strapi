c4765202d71ab40d97763d57aa37dcc3da53ff86
Paulo Ziembick <pauloziembick@gmail.com>
2024-06-17 22:16:02 -0300

    pasta out

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/rode-strapi' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/rode-strapi/' : '',
   output: 'export',
   trailingSlash: true,
   images: {
@@ -21,6 +19,33 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: process.env.NODE_ENV === 'production' ? '/rode-strapi' : '',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/rode-strapi/' : '',
+//   output: 'export',
+//   trailingSlash: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.ctfassets.net',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//     unoptimized: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+// export default nextConfig;
 
 
 // hostname: "127.0.0.1",
