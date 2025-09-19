5cd334521ee0111f339ae7bfcdd94c9c1dc62f5d
Paulo Ziembick <pauloziembick@gmail.com>
2025-09-19 19:22:31 -0300

    feat: added analitcs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import AppProviders from "./AppProviders";
 import WhatsAppBtn from "./components/whatsappBtn";
 import { GoogleTagManager } from '@next/third-parties/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from "@vercel/analytics/next"
 
 
 
